a76190da2e4906ffe68848055da79443942da088
Nazlı Kaymaz <kaymaznazli4321.1@gmail.com>
2023-06-14 01:36:23 +0300

    Add project files.

---
+++ Udemy.AdvertisementApp.Entities/AdvertisementAppUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy.AdvertisementApp.Entities
+{
+    public class AdvertisementAppUser
+    {
+        public int AdvertisementID { get; set; }
+        public Advertisement Advertisement { get; set; }
+        public int AppUserID { get; set; }
+        public AppUser AppUser { get; set; }
+        public int AdvertisementAppUserStatusID { get; set; }
+        public AdvertisementAppUserStatus AdvertisementAppUserStatus { get; set; }
+        public int MilitaryStatusID { get; set; }
+        public MilitaryStatus MilitaryStatus { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int WorkExperience { get; set; }
+        public string CvPath { get; set; }
+    }
+}
---
+++ Udemy.AdvertisementApp.Entities/AppUserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy.AdvertisementApp.Entities
+{
+    public class AppUserRole : BaseEntity
+    {
+        public int AppUserID { get; set; }
+        public AppUser AppUser { get; set; }
+        public int AppRoleID { get; set; }
+        public AppRole AppRole { get; set; }
+    }
+}
